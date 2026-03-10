5a0d37109013853db7dc05c4cf8c6b52cc7c6b57
codebot-robot <codebot-robot@google.com>
2026-03-10 18:30:20 +0000

    Fix gofmt formatting in main.go

--- cmd/gateway-api-reference-implementation/main.go
+++ cmd/gateway-api-reference-implementation/main.go
@@ -153,7 +153,7 @@ func run(ctx context.Context) error {
 			Addr:    proxyHTTPSAddr,
 			Handler: p,
 			TLSConfig: &tls.Config{
-				Certificates:        []tls.Certificate{cert},
+				Certificates:       []tls.Certificate{cert},
 				GetConfigForClient: p.GetConfigForClient,
 			},
 		}
